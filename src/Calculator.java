2da3a6090e3795786048062bde0899017556adeb
van thoai nguyen <nguyenvanthoai98@outlook.com>
2021-09-07 20:25:49 +0100

    Added choices of operations

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Welcome to the calculator!");
+        Random random = new Random();
+        int ran1 = random.nextInt(100);
+        int ran2 = random.nextInt(100);
+        System.out.println("The two randomly generated numbers are: "
+        + ran1 + ", "+ ran2);
+        System.out.println("Please enter operation: +, *, -");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        System.out.print("The result is: ");
+
+        if (input.equals("+")) {
+            System.out.println(ran1 + ran2);
+        }
+
+        if (input.equals("*")) {
+            System.out.println(ran1 * ran2);
+        }
+
+        if (input.equals("-")) {
+            System.out.println(ran1 - ran2);
+        }
+
     }
+
+
 }
